41d947987718686fbc02c5cbe3c8ff2558b8e9be
S0F1AX <107075867+S0F1AX@users.noreply.github.com>
2022-10-20 20:12:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+nomeConvidados=[];
+var nomeConvidados = document.getElementById("NomeConvidados1").value;
+NomeConvidados.push(nomeConvidados);
+NomeConvidados.sort();
+
+function searching()
+{
+    var s= document.getElementById("s1").value;
+    var found=0;
+    var i:
+    for(i=0; i<nomeConvidados.legth; i++)
+    document.getElementById(p2).innerHTML="Nome encontrado"+found+"vez(es)";
+    console.log("Nome encontrado"+found+"vez(es)");
+}
